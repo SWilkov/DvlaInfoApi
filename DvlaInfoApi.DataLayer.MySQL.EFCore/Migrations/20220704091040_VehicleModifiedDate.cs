8e2306c6af694fb1574c387518e17bd65db1db97
Scott Williamson <scott.williamson@odds-bods.com>
2022-07-04 10:12:32 +0100

    Added modifiedat

---
+++ DvlaInfoApi.DataLayer.MySQL.EFCore/Migrations/20220704091040_VehicleModifiedDate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DvlaInfoApi.DataLayer.MySQL.EFCore.Migrations
+{
+    public partial class VehicleModifiedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "modified_at",
+                table: "Vehicles",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "modified_at",
+                table: "dvlaInfos",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "modified_at",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "modified_at",
+                table: "dvlaInfos");
+        }
+    }
+}
